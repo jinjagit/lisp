273c73a12f116234c4703d6dee486cb9e38675bd
Simon Tharby <jinjamail@gmail.com>
2020-07-24 22:42:15 +0100

    hello world

---
+++ test.lisp
@@ -0,0 +1,11 @@
+#!/usr/bin/sbcl --script
+
+;;;; some LISP basics
+;;; note bash path as first line
+;;  grant file permissions. e.g. chmod 0755 test.lisp 
+; run as for bash file: ./test.lisp
+#||
+multiline comment
+||#
+
+(format t "Hello World ~%") ; it lives!
