b96180b105ccfbb9287f6bb6a0de0f5654da9721
Simon Tharby <jinjamail@gmail.com>
2020-07-25 01:12:50 +0100

    remove space

--- hello_world.lisp
+++ hello_world.lisp
@@ -2,7 +2,7 @@
 
 ;;;; some LISP basics
 ;;; note bash path in first line
-;;  grant file permissions. e.g. chmod 0755 test.lisp 
+;; grant file permissions. e.g. chmod 0755 test.lisp 
 ; run as for bash file: ./test.lisp
 #||
 multiline comment
