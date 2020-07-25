525bab3a7efbaff251f80995b54f92da9d2c5325
Simon Tharby <jinjamail@gmail.com>
2020-07-25 22:23:30 +0100

    loops

---
+++ loops.lisp
@@ -0,0 +1,41 @@
+#!/usr/bin/sbcl --script
+
+;;;; loops
+
+(loop for x from 1 to 10 do
+  (print x))
+
+(terpri) ; print newline
+(terpri) ; print newline
+
+
+(defvar i 1)
+
+(loop
+  (format t "~d ~%" i)
+  (setq i (+ i 1))
+  (when (> i 10) (return i)))
+
+(terpri) ; print newline
+
+
+(loop for j in '(Peter Paul Mary) do
+  (format t "~d ~%" j))
+
+(terpri) ; print newline
+
+
+(loop for i from 100 to 110 do
+  (print i))
+
+(terpri) ; print newline
+(terpri) ; print newline
+
+
+(dotimes (i 10) ; starts at i = 0
+  (print i))
+
+(terpri) ; print newline
+(terpri) ; print newline
+
+
