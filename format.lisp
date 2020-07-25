4fc39f4f41a18d718623565977c5fa795dd21cff
Simon Tharby <jinjamail@gmail.com>
2020-07-25 13:01:09 +0100

    add format.lisp

---
+++ format.lisp
@@ -0,0 +1,13 @@
+#!/usr/bin/sbcl --script
+
+;;;; formats of data values
+
+(format t "Number with commas ~:d ~%" 10000000) ; => 10,000,000
+
+(format t "PI to 5 characters ~5f ~%" 3.141593) ; => 3.142
+
+(format t "PI to 4 decimals ~,4f ~%" 3.141593) ; => 3.1416
+
+(format t "10 percent ~,,2f ~%" .10) ; => 10.0
+
+(format t "10 Dollars ~$ ~%" 10) ; => 10.00
