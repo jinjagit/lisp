d645f04920ae0d82b86e9d3a88e59f2eb07b2a22
Simon Tharby <jinjamail@gmail.com>
2020-07-26 14:45:10 +0100

    functions.lisp

--- functions.lisp
+++ functions.lisp
@@ -67,9 +67,13 @@
 
 ;; quasi-quoting allows switching from code to data mode:
 
-(defparameter *heroes*
-  '((Superman (Clark Kent))
-  (Flash (Barry Allen))
-  (Batman (Bruce wayne))))
+(defparameter *hero-size*
+  '((Superman (6ft 3in) (230lbs))
+  (Flash (6ft 0in) (190lbs))
+  (Batman (6ft 2in) (210lbs))))
 
-(defun get-hero-data (hero-name))
+(defun get-hero-data (size)
+  (format t "~a ~%"
+  `(,(caar size) is ,(cadar size) and ,(cddar size))))
+
+(get-hero-data *hero-size*) ; => (Superman Is (6ft 3in) And ((230lbs)))
